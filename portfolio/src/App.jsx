51553e2d336a1f7c952522c1efcfa61a026a8aff
rohandatr <rohandatar2003@gmail.com>
2024-07-09 16:43:22 +0530

    new section added

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function App() {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-300'>
-      <div className='flixed top-0 -z-10 h-full w-full'>
+      <div className='fixed top-0 -z-10 h-full w-full'>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 
       bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       </div>
@@ -11,6 +11,7 @@ function App() {
       <div className='container mx-auto px-8'>
       <Navbar />
       <Hero />
+      <About />
       </div>
     </div>
   )
